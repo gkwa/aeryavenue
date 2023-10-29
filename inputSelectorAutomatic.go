3de0f8771382551f1e5f0c622b940275a1ac8503
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-29 15:57:21 -0700

    Fix case where item list is empty

--- inputSelectorAutomatic.go
+++ inputSelectorAutomatic.go
@@ -3,5 +3,9 @@ package aeryavenue
 type AutomaticItemInputSelector struct{}
 
 func (selector *AutomaticItemInputSelector) SelectItem(listItems []string) (string, error) {
+	if len(listItems) < 1 {
+		return "", nil
+	}
+
 	return listItems[0], nil
 }
